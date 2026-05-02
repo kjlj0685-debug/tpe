0bfe509ab3d56433e076f7aa8d4ac05a8d471150
kenza <kjlj0685@gmail.com>
2026-05-02 09:49:55 +0100

    Refactor code: extracted methods, improved naming, added unit tests

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,23 +1,31 @@
 package org.example;
 
 public class Calculator {
-    public double calculateSumAndProductRatio(double firstOperand, double secondOperand) {
-        double sum = firstOperand + secondOperand;
-        double product = firstOperand * secondOperand;
+    public double calculateSum(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    public double calculateProduct(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+
+    public double calculateRatioOfSumToProduct(double firstOperand, double secondOperand) {
+        double sum = calculateSum(firstOperand, secondOperand);
+        double product = calculateProduct(firstOperand, secondOperand);
         return sum / product;
     }
 
-    public void printResult(double result) {
-        System.out.println("Result: " + result);
+    public double calculateAverage(double firstOperand, double secondOperand) {
+        return calculateSum(firstOperand, secondOperand) / 2.0;
     }
 
-    public double calculateAverage(double firstOperand, double secondOperand) {
-        return (firstOperand + secondOperand) / 2.0;
+    public void displayResult(double result) {
+        System.out.println("Result: " + result);
     }
 
     public static void main(String[] args) {
         Calculator c = new Calculator();
-        double result = c.calculateSumAndProductRatio(2, 3);
-        c.printResult(result);
+        double result = c.calculateRatioOfSumToProduct(2, 3);
+        c.displayResult(result);
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -6,6 +6,33 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
     @Test
+    void testCalculateSum() {
+        Calculator calculator = new Calculator();
+
+        double result = calculator.calculateSum(4, 6);
+
+        assertEquals(10.0, result);
+    }
+
+    @Test
+    void testCalculateProduct() {
+        Calculator calculator = new Calculator();
+
+        double result = calculator.calculateProduct(4, 6);
+
+        assertEquals(24.0, result);
+    }
+
+    @Test
+    void testCalculateRatioOfSumToProduct() {
+        Calculator calculator = new Calculator();
+
+        double result = calculator.calculateRatioOfSumToProduct(2, 3);
+
+        assertEquals((2 + 3) / (2.0 * 3.0), result, 1e-9);
+    }
+
+    @Test
     void testCalculateAverage() {
         Calculator calculator = new Calculator();
 
